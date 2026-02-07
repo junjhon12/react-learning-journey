7eda95de3e7cd9a9791b4bef1deaba1b0c7d9cf0
junjhon12 <q.le102100@gmail.com>
2026-02-07 17:42:17 -0500

    refactor(App): improve structure and component logic

--- dev-folio/src/App.jsx
+++ dev-folio/src/App.jsx
@@ -1,22 +1,31 @@
 import Navbar from './components/Navbar'
 import ProjectCard from './components/ProjectCard';
+import { useState } from 'react';
 
 const projectDatas = [
-    {id:1, title:"project data 1 title", description:"project data description 1", link:"#"},
-    {id:2, title:"project data 2 title", description:"project data description 2", link:"#"},
-    {id:3, title:"project data 3 title", description:"project data description 3", link:"#"}
+    {id:1, title:"project data 1 title", description:"project data description 1", link:"#", category:"React"},
+    {id:2, title:"project data 2 title", description:"project data description 2", link:"#", category:"Node"},
+    {id:3, title:"project data 3 title", description:"project data description 3", link:"#", category:"MERN"}
   ];
 function App() {
+  const [filter, setFilter] = useState('All');
+  const filteredProjects = filter === 'All' ? projectDatas : projectDatas.filter(p => p.category == filter)
   return (
     <div>
       <Navbar/>
+      <div>
+        <button type="button" value={"MERN"} onClick={() => setFilter("MERN")}>MERN</button>
+        <button type="button" value={"React"} onClick={() => setFilter("React")}>React</button>
+        <button type="button" value={"Node"} onClick={() => setFilter("Node")}>Node</button>
+      </div>
       <main>
-        {projectDatas.map( (projectData) => (
+        {filteredProjects.map( (projectData) => (
           <ProjectCard 
           key={projectData.id}
           title={projectData.title} 
           description={projectData.description} 
-          link={projectData.link}/>
+          link={projectData.link}
+          category={projectData.category}/>
         ))}
       </main>
     </div>
